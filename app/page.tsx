86c0fd7c1ce1ab74d12222882740d67a66882c9b
harleen001 <harleen.johal31@gmail.com>
2025-07-08 21:09:43 +0530

    navbar-updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 import HomePage from "@/components/Homepage";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+
 
 export default function Home() {
   return (
